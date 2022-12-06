08e7a0818537dc79a8e61d3b5562590fc952fc0a
ArcanaSJ <seanjosepharcana@gmail.com>
2022-12-06 22:29:37 +0800

    Commit 11 : Academics

--- Project/JavaScript/jsLogin.js
+++ Project/JavaScript/jsLogin.js
@@ -1,8 +1,8 @@
 function auth(){
     var user = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(user == "admin" && password == "admin"){
-        window.location.assign("myHome.html");
+    var pass = document.getElementById("password").value;
+    if(user == "admin" && pass == "admin"){
+        window.location.href = "myHome.html";
         alert("Login Successfully!");
     } else {
         alert("Invalid Password or Username!");
