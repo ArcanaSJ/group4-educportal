ac742728568e1aa6d668e265d2752ecfe54e8c7b
ArcanaSJ <seanjosepharcana@gmail.com>
2022-12-10 01:32:17 +0800

    Commit 19 : JavaScript to Login

--- Project/JavaScript/jsLogin2.js
+++ Project/JavaScript/jsLogin2.js
@@ -3,10 +3,22 @@ function auth(){
     var pass = document.getElementById("password").value;
     var mail = document.getElementById("email").value;
     if(user == "admin" && pass == "admin" && mail == "admin@gmail.com"){
-        window.location.href = "myHome.html";
+        window.location.href = "https://forms.gle/NnmfjwEtNnqWJpMp9";
         alert("Login Successfully!");
     } else {
         alert("Invalid Password or Username!");
+        return;
+    }
+}
+
+function reg(){
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    var mail = document.getElementById("email").value;
+    if(user == "admin" && pass == "admin" && mail == "admin@gmail.com"){
+        alert("Registered Successfully!");
+        location.reload();
+    } else {
         return;
     }
 }
